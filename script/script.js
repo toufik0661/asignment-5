851360610f73f4ac1f9485809afedf90718f1c09
toufik0661 <toufiqurrahman0661@gmail.com>
2025-03-03 19:08:15 +0600

    add current date and time

--- script/script.js
+++ script/script.js
@@ -9,6 +9,29 @@ document.getElementById('discover-something').addEventListener('click', function
   window.location.href = 'bloge.html';
 });
 
+//date and time 12 hour format
+const date = new Date();
+const time = date.toLocaleString('en-US', {
+  hour: 'numeric',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: true
+});
+const hour = date.getHours();
+const minute = date.getMinutes();
+const second = date.getSeconds();
+const day = date.toLocaleDateString('en-GB', { day: '2-digit' })
+const dayName = date.toLocaleString('en-GB', { weekday: 'short' });
+const month = date.toLocaleString('en-GB', { month: 'long' });
+const year = date.getFullYear();
+document.getElementById('dayName').innerHTML = `
+${dayName},
+`;
+document.getElementById('dayName-and-date').innerHTML =
+`${month}, ${day}, ${year}`;
+
+
+
 
 
 
